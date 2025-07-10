24c0866f974f70e6f085731b13b73e1f5b5122db
ภูริพัฒน์ ศักดิ์นุกูล <phuriphat1793@hotmail.com>
2025-07-11 01:57:56 +0700

    Remove student and teacher management files
    - Deleted student_list.php, student_list_edit.php, student_list_edit_db.php, student_search.php, teacher_del_db.php, teacher_edit.php, teacher_edit_db.php, teacher_home.php, teacher_info.php, teacher_list.php, teacher_list_edit.php, teacher_list_edit_db.php, teacher_search.php, teacher_student_course.php, teacher_student_list.php, and teacher_student_search.php.
    - This cleanup is part of a refactoring effort to streamline the codebase and improve maintainability.

--- condb.php
+++
@@ -1,5 +0,0 @@
-<?php
-$con= mysqli_connect("localhost","root","","advisor-g5") or die("Error: " . mysqli_error($con));
-mysqli_query($con, "SET NAMES 'utf8' ");
-  date_default_timezone_set('Asia/Bangkok');
-?>
