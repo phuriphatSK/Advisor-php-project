5a5c5f029931a60d13b146336d670d3057c2297e
ภูริพัฒน์ ศักดิ์นุกูล <phuriphat1793@hotmail.com>
2025-07-13 21:30:04 +0700

    Remove Composer autoload files and related license information to clean up vendor directory

--- database/condb.php
+++ database/condb.php
@@ -1,16 +1,5 @@
 <?php
-require_once __DIR__ . '../../vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-
-$host = $_ENV['DB_HOST'];
-$port = $_ENV['DB_PORT'];
-$db   = $_ENV['DB_NAME'];
-$user = $_ENV['DB_USER'];
-$pass = $_ENV['DB_PASS'];
-
-$con = mysqli_connect($host, $user, $pass, $db, (int)$port);
+$con = mysqli_connect("localhost", "root", "", "advisor-g5", 3307);
 
 if (mysqli_connect_errno()) {
     die("Failed to connect to MySQL: " . mysqli_connect_error());
